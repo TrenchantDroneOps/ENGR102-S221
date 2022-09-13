b83d5f2b4729423be076008ab37f1a4d2161b656
TrenchantDroneOps <76067494+TrenchantDroneOps@users.noreply.github.com>
2022-09-13 11:11:26 -0500

    Add files via upload

---
+++ unit_conversions.py
@@ -0,0 +1,34 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:       Zane Akers
+# Section:      221
+# Assignment:   Lab 3.15
+# Date:         8 September 2022
+
+
+#a) Pounds (force) to Newtons
+#b) Meters to feet
+#c) Atmospheres to kilopascals (kPa)
+#d) Watts to BTU per hour
+#e) Liters per second to US gallons per minute
+#f) Degrees Celsius to degrees Fahrenheit
+
+x = float(input("Please enter the quantity to be converted:"))
+#####------(a)--------#########
+print(x, "pounds force is equivalent to ", round(x/0.22480894387, 2), "Newtons")
+
+#####------(b)--------#########
+print(x, "meters is equivalent to ", round(x*3.2808399, 2), "feet")
+
+#####------(c)--------#########
+print(x, "atmospheres is equivalent to ", round(2,x*), "kilopascals")
+
+
+
+
+
+
+
+
