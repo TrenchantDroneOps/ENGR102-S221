5fd716c010e2c70fd155c0c230bf4ad4cd3235a4
davidguess21 <113459055+davidguess21@users.noreply.github.com>
2022-09-13 11:13:10 -0500

    Add files via upload

---
+++ still_more_linear_interpolation.py
@@ -0,0 +1,34 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         David Guess
+# Section:      221
+# Assignment:   Lab 2.11.1
+# Date:         7 9 2022
+#
+
+from math import *
+#hi
+
+#x0, y0, z0 are starting positions
+#x1, y1, z1 are ending positions
+#t0 is starttime, t1 is end time
+
+
+t0 = float(input("Enter time 1: "))
+x0 = float(input("Enter the x position of the object at time 1: "))
+y0 = float(input("Enter the x position of the object at time 1: "))
+z0 = float(input("Enter the x position of the object at time 1: "))
+
+t1 = float(input("Enter time 2: "))
+x1 = float(input("Enter the x position of the object at time 2: "))
+y1 = float(input("Enter the x position of the object at time 2: "))
+z1 = float(input("Enter the x position of the object at time 2: "))
+print()
+
+
+print("At time", f't0 is {t0:.2f}', "seconds the object is at", (f'x0 is {x0:.3f}', f'y0 is {y0:.3f}', f'z0 is {z0:.3f}'))
+
+
+
