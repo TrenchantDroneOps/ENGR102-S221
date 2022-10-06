fa0155d2a9deade6b04fb843fddf9f7a721c39d8
davidguess21 <113459055+davidguess21@users.noreply.github.com>
2022-10-05 23:48:25 -0500

    Add files via upload

--- pyramid_area2.py
+++ pyramid_area2.py
@@ -1,3 +1,4 @@
+
 # By submitting this assignment, I agree to the following:
 #   "Aggies do not lie, cheat, or steal, or tolerate those who do."
 #   "I have not given or received any unauthorized aid on this assignment."
@@ -13,19 +14,16 @@ from math import *
 
 side = float(input("Enter the side length in meters: "))
 
-layer = int(input("Enter the number of layers in meters: "))
+layer = int(input("Enter the number of layers: "))
 
 
-area = 3*(layer**2 - layer)*(side**2) + (sqrt(3)/4)*((layer*side)**2)
+num_squares = (layer/2*(2 + (layer-1)))*3
+
+area = num_squares*(side**2)
+
+top = 0.25*sqrt(3)*(side*layer)**2
 
 
-print(f'You need {area:.2f} m^2 of gold foil to cover the pyramid')
+print(f'You need {area+top:.2f} m^2 of gold foil to cover the pyramid')
 
-
-
-
-
-
-
-
-
+#cool it works
