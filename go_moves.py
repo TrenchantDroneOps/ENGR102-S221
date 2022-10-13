20569eb3794e419b0f9a7c819e13a2513759f5d9
TrenchantDroneOps <76067494+TrenchantDroneOps@users.noreply.github.com>
2022-10-12 22:18:34 -0500

    Add files via upload

---
+++ go_moves.py
@@ -0,0 +1,20 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Nicholas Griffin, Zane Akers, David Guess, Sraavya Danala
+# Section:      221
+# Assignment:   Lab 7
+# Date:         12 October 2022
+#
+
+Board = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]]
+
+for row in Board:
+    print()
+    for column in row:
+        print(".", end=" ")
+
+
+
+
